13bff91690fe4b148b1a3792ef002a8f6b4951e7
ynhea <155337921+ynhea@users.noreply.github.com>
2024-10-06 08:24:38 +0900

    Update Server.java

--- CETA/src/community/Server.java
+++ CETA/src/community/Server.java
@@ -1,9 +1,9 @@
 package community;
 
 import java.io.BufferedReader;		// 빨리 읽어들이기
-import java.io.IOException;			// 입출력 예외
+import java.io.IOException;		// 입출력 예외
 import java.io.InputStreamReader;	// 문자단위의 데이터로 변환
-import java.io.PrintWriter;			// 문자단위로 출력
+import java.io.PrintWriter;		// 문자단위로 출력
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -12,26 +12,30 @@ import java.util.List;
 import main.Menu;
 
 public class Server {
-	// ※ 서버 생성 (socket + thread)
+	// ※ 메뉴와 연결된 start()함수
 	public void start() {
 		// 소켓(socket) = 인터넷을 경유하도록 설계된 스트림(컴퓨터 네트워크에서 데이터를 주고받을 수 있는 창구 역할)
 		ServerSocket serverSocket = null;
 		Socket socket = null;
 		try {
-			serverSocket = new ServerSocket(7000);	// 클라이언트의 요청을 받기 위한 준비를 한다
+			// (1) 서버 소켓 생성
+			serverSocket = new ServerSocket(7000);
 			while(true) {
+				// (2) 요청이 오면 클라이언트와 통신하는 새 소켓을 accept()를 활용해 만듦
 				System.out.println("[클라이언트 연결대기중]");
-				socket = serverSocket.accept();		// 클라이언트의 요청을 받아들인다
-				ReceiveThread receiveThread = new ReceiveThread(socket);	// 서버-클라이언트의 통신을 처리하는 새로운 스레드를 생성한다
+				socket = serverSocket.accept();
+				// (3) 서버가 클라이언트의 메세지를 받기위한 스레드 생성 & 실행
+				ReceiveThread receiveThread = new ReceiveThread(socket);
 				receiveThread.start();
+				// run(): 호출스택이 생성되지 않아 쓰레드가 독립적으로 수행되지 않음 -> start()사용!
 			}
-		} catch (IOException e) { e.getStackTrace(); }	// 예외처리
+		} catch (IOException e) { e.getStackTrace(); }
 	}
 }
 
-// ※ 서버가 클라이언트의 메세지를 받기 위한 새로운 스레드를 생성하는 클래스
+// ※ 스레드를 생성하는 클래스
 class ReceiveThread extends Thread{
-	// ※ 필드
+	// (1-1) 클라이언트에게 메시지를 보내기위해 (Printwriter객체들을 저장하는) 동기화된 arrayList 생성
 	static List<PrintWriter> list = Collections.synchronizedList(new ArrayList<PrintWriter>());
 	// - ArrayList: 배열 기반 리스트, 크기가 가변임: List<> list = new ArrayList<>();
 	// - PrintWriter: 데이터를 문자로 파일에 기록하거나 인쇄하는 클래스
@@ -41,7 +45,7 @@ class ReceiveThread extends Thread{
 	BufferedReader in = null;
 	PrintWriter out = null;
 	
-	// ※ 생성자
+	// (1-2) 생성자에서, 입출력스트림 생성 & 동기화 리스트에 저장
 	public ReceiveThread (Socket socket) {
 		this.socket = socket;
 		try {
@@ -53,26 +57,27 @@ class ReceiveThread extends Thread{
 		}
 	}
 	
-	// ※ 채팅방 입장-채팅-접속끊김-퇴장 함수
+	// (2) run() 재정의 : try-with-resourse를 사용해 자원관리, 메세지 보내기 과정을 수행
 	public void run() {
 		String name = "";
-		// - 입장&채팅: 이름읽기 - sendAll - !null,메세지 sendAll
+		// - 자원관리
 		try {
+			// - 입장시, 이름 보내기
 			name = in.readLine();
 			System.out.println("[" + name + " 새 연결 생성]");
 			sendAll("[" + name + "]님이 들어오셨습니다.");
-			
+			// - 메세지 보내기
 			while (in != null) {
 				String inputMsg = in.readLine();
 				if ("quit".equals(inputMsg)) break;
 				sendAll(name + ">>" + inputMsg);
 			}
 		}
-		// - 예외: 접속끊김
+		// - 접속이 끊기는 예외처리
 		catch (IOException e) {
 			System.out.println("[" + name +" 접속끊김]");
 		}
-		// - 퇴장: 나갔다고 sendAll - list에서 제거 - socket.close() - 서버에 print
+		// - 퇴장시, 이름 보내기 & 동기화된 리스트에서 제거
 		finally {
 			sendAll("[" + name +"]님이 나가셨습니다.");
 			list.remove(out);
@@ -84,8 +89,8 @@ class ReceiveThread extends Thread{
 		System.out.println("[" + name +" 연결종료]");
 	}
 	
-	// ※ 전원에게 메세지를 보내는 함수
-	// - for( A : B )구문 = B에서 차례대로 객체를 꺼내 A에 넣겠다는 의미
+	// - 전원에게 메세지를 보내는 함수
+	//   -> for( A : B )구문 = B에서 차례대로 객체를 꺼내 A에 넣겠다는 의미
 	public void sendAll(String s) {
 		for (PrintWriter out: list) {
 			out.println(s);
